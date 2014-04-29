651dcc0e15ffd61fe2cf95833298a947870cdc89
noeuli <noeuli@gmail.com>
2014-04-29 18:29:35 +0900

    .

--- src/com/noeuli/fatapp/MainActivity.java
+++ src/com/noeuli/fatapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.noeuli.fatapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.CalendarView;
 
 public class MainActivity extends Activity {
+    
+    private CalendarView mCalendarView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mCalendarView = (CalendarView) findViewById(R.id.calendar);
 	}
 
 	@Override
