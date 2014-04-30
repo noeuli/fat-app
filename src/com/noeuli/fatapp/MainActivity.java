57c68ffdaf0b4c1f1ed7a77e45e6cbcfd67d8d8f
noeuli <noeuli@gmail.com>
2014-04-30 16:12:49 +0900

    Setup some CalendarView apis.
    API level changed from11 to 16.
    initCalendar() method added.

--- src/com/noeuli/fatapp/MainActivity.java
+++ src/com/noeuli/fatapp/MainActivity.java
@@ -1,11 +1,20 @@
 package com.noeuli.fatapp;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.CalendarView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+    private static final boolean LOGD = true;
+    private static final String TAG = "MainActivity";
     
     private CalendarView mCalendarView;
 
@@ -14,7 +23,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		mCalendarView = (CalendarView) findViewById(R.id.calendar);
+		initCalendar();
 	}
 
 	@Override
@@ -23,5 +32,28 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private void initCalendar() {
+        mCalendarView = (CalendarView) findViewById(R.id.calendar);
+        mCalendarView.setFirstDayOfWeek(Calendar.SUNDAY);
+        mCalendarView.setFocusedMonthDateColor(Color.BLACK);
+        mCalendarView.setUnfocusedMonthDateColor(Color.GRAY);
+        mCalendarView.setSelectedWeekBackgroundColor(Color.CYAN);
+        mCalendarView.setShowWeekNumber(false);
+        mCalendarView.setWeekSeparatorLineColor(Color.RED);
+        mCalendarView.setOnClickListener(this);
+	}
+
+    @Override
+    public void onClick(View v) {
+        if (v==null) return;
+        int id = v.getId();
+        Log.d(TAG, "onClick() view=" + v + " id=" + id);
+        
+        if (id == R.id.calendar) {
+            long selectedDate = mCalendarView.getDate();
+            Log.d(TAG, "onClick(): date=" + new Date(selectedDate));
+        }
+    }
 
 }
