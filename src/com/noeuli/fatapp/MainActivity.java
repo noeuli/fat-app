2b9ac6aff5d432aed595429996b01672c3ceefc8
noeuli <noeuli@gmail.com>
2014-05-07 18:27:03 +0900

    test codes

--- src/com/noeuli/fatapp/MainActivity.java
+++ src/com/noeuli/fatapp/MainActivity.java
@@ -1,29 +1,95 @@
 package com.noeuli.fatapp;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import android.app.Activity;
-import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.CalendarView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
     private static final boolean LOGD = true;
     private static final String TAG = "MainActivity";
     
-    private CalendarView mCalendarView;
-
+    private TextView mTextMonthTitle;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		initCalendar();
+	
+		setupViews();
+	}
+	
+	private void setupViews() {
+	    measureScreenSize();
+	    setupMonthTitle();
+	    setupDayOfWeekTitle();
+	    setupMonthCalendar();
+	}
+	
+	private int mScreenWidth;
+	private int mScreenHeight;
+	private int mWindowWidth;
+	private int mWindowHeight;
+	private int mCalendarWidth;
+	private int mCalendarHeight;
+	private int mDayWidth;
+	private int mDayHeight;
+	
+	private void measureScreenSize() {
+	    Display disp = getWindowManager().getDefaultDisplay();
+	    
+	    View v = getWindow().getDecorView();
+	    mWindowWidth = v.getWidth();
+	    mWindowHeight = v.getHeight();
+	    
+	    Rect r = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+	    Log.d(TAG, "measureScreenSize() baseLayout=" + mWindowWidth + "x" + mWindowHeight + " r=" + r);
+	}
+	
+	private void setupMonthTitle() {
+        mTextMonthTitle = (TextView) findViewById(R.id.txtMonthTitle);
+        String dateTime = DateFormat.getDateInstance().format(new Date());
+        mTextMonthTitle.setText(dateTime);
+	}
+	
+	private void setupDayOfWeekTitle() {
+	    Calendar calendar = Calendar.getInstance();
+	    LinearLayout mDayOfWeekLayout = (LinearLayout) findViewById(R.id.day_of_week_title);
+	    
+	    for (int i=0; i<7; i++) {
+	        TextView tv = new TextView(this);
+	        tv.setText(calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.KOREA));
+	        mDayOfWeekLayout.addView(tv);
+	    }
+	}
+	
+	private void setupMonthCalendar() {
+	    
+	}
+	
+	private void datePrintTest() {
+        DateFormat[] formats = new DateFormat[] {
+            DateFormat.getDateInstance(),
+            DateFormat.getDateTimeInstance(),
+            DateFormat.getTimeInstance(),
+        };
+        for (DateFormat df : formats) {
+            System.out.println(df.format(new Date()));
+            df.setTimeZone(TimeZone.getTimeZone("UTC"));
+            System.out.println(df.format(new Date()));
+        }
 	}
 
 	@Override
@@ -33,27 +99,11 @@ public class MainActivity extends Activity implements OnClickListener {
 		return true;
 	}
 	
-	private void initCalendar() {
-        mCalendarView = (CalendarView) findViewById(R.id.calendar);
-        mCalendarView.setFirstDayOfWeek(Calendar.SUNDAY);
-        mCalendarView.setFocusedMonthDateColor(Color.BLACK);
-        mCalendarView.setUnfocusedMonthDateColor(Color.GRAY);
-        mCalendarView.setSelectedWeekBackgroundColor(Color.CYAN);
-        mCalendarView.setShowWeekNumber(false);
-        mCalendarView.setWeekSeparatorLineColor(Color.RED);
-        mCalendarView.setOnClickListener(this);
-	}
-
     @Override
     public void onClick(View v) {
         if (v==null) return;
         int id = v.getId();
         Log.d(TAG, "onClick() view=" + v + " id=" + id);
-        
-        if (id == R.id.calendar) {
-            long selectedDate = mCalendarView.getDate();
-            Log.d(TAG, "onClick(): date=" + new Date(selectedDate));
-        }
     }
 
 }
